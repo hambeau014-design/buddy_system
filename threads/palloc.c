5f4a3369347003d19aeb4614fc8e948df98908e4
hambeau014-design <hambeau014@gmail.com>
2025-12-07 00:54:22 +0900

    Update palloc.c

--- threads/palloc.c
+++ threads/palloc.c
@@ -24,9 +24,6 @@
    By default, half of system RAM is given to the kernel pool and
    half to the user pool.  That should be huge overkill for the
    kernel pool, but that's just fine for demonstration purposes. */
-//[추가]
-static size_t palloc_next_fit_scan(struct pool *pool, size_t page_cnt);
-static size_t palloc_best_fit_scan(struct pool *pool, size_t page_cnt);
 
 /* A memory pool. */
 struct pool {
