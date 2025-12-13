2dd1f7ca6d5752ed6105e57c0578cab57252b3a0
hambeau014-design <hambeau014@gmail.com>
2025-12-13 23:13:45 +0900

    Update palloc.c

--- threads/palloc.c
+++ threads/palloc.c
@@ -11,6 +11,8 @@
 #include "threads/synch.h"
 #include "threads/vaddr.h"
 
+#define MIN(a, b) ((a)<(b) ? (a):(b))
+
 /* Page allocator.  Hands out memory in page-size (or
    page-multiple) chunks.  See malloc.h for an allocator that
    hands out smaller chunks.
