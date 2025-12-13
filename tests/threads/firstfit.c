69530c58c727317f1cd5fe2fff632e407e275552
hambeau014-design <hambeau014@gmail.com>
2025-12-14 01:55:29 +0900

    Update firstfit.c

--- tests/threads/firstfit.c
+++ tests/threads/firstfit.c
@@ -14,24 +14,22 @@ test_firstfit (void)
     void *pB = palloc_get_multiple (PAL_ASSERT, 2);
     void *pC = palloc_get_multiple (PAL_ASSERT, 1);
 
-    msg("A(1P), B(2P), C(1P) 할당 완료.");
+    msg("|A(1P)| B(2P) | C(1P)| 할당");
     
     // 2. 중앙 블록 B 해제하여 틈새 생성: A [사용] -> B [여유 2P] -> C [사용]
     palloc_free_multiple (pB, 2);
-    msg("중앙 블록 B (2페이지) 해제, 틈새 생성.");
+    msg("블록 B (2페이지) 해제====| A(1P) |     | C(1P)|");
 
     // 3. First-Fit 핵심 테스트
     // 요청: 1 페이지
-    // pA 다음인 B의 2페이지 틈새가 '가장 먼저' 발견되어야 합니다.
     void *pD = palloc_get_multiple (PAL_ASSERT, 1); 
 
-    // 검증: pD의 주소가 해제된 pB의 시작 주소와 같아야 합니다.
     if (pD != pB) {
-        // 만약 pD가 pC 뒤의 다른 공간에 할당되었다면 오류입니다.
-        fail("First-Fit 로직 오류: pD가 첫 번째 틈새(pB의 자리)에 할당되지 않음.");
+        // 만약 pD가 pC 뒤의 다른 공간에 할당되었다면 오류
+        fail("First-Fit 로직 오류: 첫 번째 틈새(pB의 자리)에 할당되지 않음.");
     }
     
-    msg("First-Fit 로직 검증 성공: pD가 pB의 자리에 할당됨.");
+    msg("First-Fit 로직 검증 성공: pB의 자리에 할당됨====| A(1P) | D(1P) | C(1P) |");
     
     palloc_free_multiple(pA, 1); 
     palloc_free_multiple(pC, 1); 
